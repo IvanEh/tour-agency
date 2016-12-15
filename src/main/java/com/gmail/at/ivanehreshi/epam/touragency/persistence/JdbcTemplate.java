130baf3634506cea3f361468cddf7ff7d20cb2c5
ivaneh <ivanehreshi@gmail.com>
2016-12-15 20:12:50 +0200

    Optimize imports

--- src/main/java/com/gmail/at/ivanehreshi/epam/touragency/persistence/JdbcTemplate.java
+++ src/main/java/com/gmail/at/ivanehreshi/epam/touragency/persistence/JdbcTemplate.java
@@ -1,10 +1,8 @@
 package com.gmail.at.ivanehreshi.epam.touragency.persistence;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
